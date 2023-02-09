398d8cdd93073ec9652325c763119f78cc1f4241
Rachit2030 <69667845+Rachit2030@users.noreply.github.com>
2023-02-09 12:37:01 +0530

    Add Flutter to iOS Communication

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,22 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+      let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+      let method_channel_name = "material.dart/battery"
+      let batteryChannel = FlutterMethodChannel(
+        name: method_channel_name,
+        binaryMessenger:controller.binaryMessenger
+        )
+      batteryChannel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult)-> Void in 
+        switch call.method {
+        case "getBatteryLevel": result("HELLO iOS")
+          
+        default: result(FlutterMethodNotImplemented) 
+          
+        }
+      })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
